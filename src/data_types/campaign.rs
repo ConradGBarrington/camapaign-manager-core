711859c89c3a07a3f2c303adaf0de518155759b8
Conrad Barrington <conradgbarrington@gmail.com>
2024-03-01 00:45:50 -0500

    Added a campaign data type

---
+++ src/data_types/campaign.rs
@@ -0,0 +1,4 @@
+pub struct Campaign {
+  pub name: String,
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+mod data_types;
+
 fn main() {
+
+    let test_campaign = data_types::campaign::Campaign{name: "test name".to_string()};
+
     println!("Hello, world!");
+    println!("{}", test_campaign.name);
 }
